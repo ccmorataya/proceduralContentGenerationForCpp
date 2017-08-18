303d7e47b197a3881fd985c67277b82be3e4865f
Cristian Morataya <cris.morataya@gmail.com>
2017-08-18 16:05:18 -0600

    Add gitignore and the randGenerator

---
+++ randomNumbers/pseudoRandom.cpp
@@ -0,0 +1,20 @@
+// @ccmorataya random number generator with the std::rand()
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        cout << "Press a key to generate a random number: ";
+        cin.get();
+
+        // CM: generate the random integer
+        int randomInteger = rand();     // can be std::rand()
+
+        cout << randomInteger << endl << endl;
+    }
+    return 0;
+}
