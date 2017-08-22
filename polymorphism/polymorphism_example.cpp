53209f3dcb53bfe16fd5aaa0a0e2c39d33318dd5
Cristian Morataya <cris.morataya@gmail.com>
2017-08-22 11:39:15 -0600

    Add missing semi colons and syntax

--- polymorphism/polymorphism_example.cpp
+++ polymorphism/polymorphism_example.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namspace std;     // replace the std::xx syntax
+using namespace std;     // replace the std::xx syntax
 
 int main()
 {
@@ -12,24 +12,24 @@ int main()
         virtual string GetFoodType()
         {
             return foodType;
-        }
-    }
+        };
+    };
 
     struct Pizza : public Food
     {
         // CM: overriding the GetFoodType method from the Food struct
-        string GetFoodType override
+        string GetFoodType() override
         {
-            itemType = "Pizza :P";
-            return itemType;
-        }
-    }
+            foodType = "Pizza :P";
+            return foodType;
+        };
+    };
 
     // CM: instantiate myFood as Pizza
     Food myFood = Pizza();
 
     // CM: print the type of myFood
-    cuot << myFood.GetFoodType().c_str() << endl;
+    cout << myFood.GetFoodType().c_str() << endl;
     cin.get();
 
     // CM: return 0 = success
